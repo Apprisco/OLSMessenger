25480837a133598b7298a5a150aa339c0499ec48
Steven G <38004233+stevenpowered@users.noreply.github.com>
2020-11-13 14:10:49 +0800

    sample code

--- src/main/java/com/olsmessenger/database/DatabaseTest.java
+++ src/main/java/com/olsmessenger/database/DatabaseTest.java
@@ -2,6 +2,8 @@ package com.olsmessenger.database;
 
 import com.olsmessenger.database.tables.User;
 
+import java.util.ArrayList;
+
 /**
  * @author : Steven Gao
  * @since : 11/13/20, Fri
@@ -17,6 +19,13 @@ public class DatabaseTest {
         user.setPassword("test");
         user.setUsername("kwoo");
         databaseInterface.addUser(user);
+        databaseInterface.getAllUsers().forEach(System.out::println);
+
+        // if you change anything in the class, save user with
+        ArrayList<String> classes = new ArrayList<>();
+        classes.add("700 mph scooter proj");
+        user.setClasses(classes);
+        databaseInterface.saveUser(user);
         databaseInterface.getAllUsers().forEach(System.out::println);
     }
 }
