6863aa932107b9afa34de5687f4dab12769fe609
CoderStone <34324909+CoderStone@users.noreply.github.com>
2020-11-21 01:53:31 -0500

    Update generateChatHTML.php

--- generateChatHTML.php
+++ generateChatHTML.php
@@ -18,9 +18,9 @@
 		$time=$_POST['time'];
 		$time=date('m/d/Y',(int)$time/1000); 
 if($resultv==$_SESSION['name'])
-$chat="<div class='d-flex justify-content-end mb-4'><div class='message_sent'>".$message."<span class='time_date'>Me,".$time." </span></div><div class='profile_picture'><img src='https://cdn.discordapp.com/attachments/582274693097193472/778992941900627998/unknown.png'class='rounded-circle user_pfp'></div></div>";
+$chat="<div class='d-flex justify-content-end mb-4'><div><div class='message_sent'>".$message."</div><div class='time_date'>Me, "."  ".$time." </div></div><div class='profile_picture'><img src='https://cdn.discordapp.com/attachments/582274693097193472/778992941900627998/unknown.png'class='rounded-circle user_pfp'></div></div>";
 else
-$chat="<div class='d-flex justify-content-start mb-4'><div class='profile_picture'><img src='https://josephchen.tech/wp-content/uploads/2020/08/test1.jpg'class='rounded-circle user_pfp'></div><div class='message_received'>".$message."<span class='time_date'>".substr($resultv,0,strrpos($resultv,' ')).", ".$time."</span></div></div>";
+$chat="<div class='d-flex justify-content-start mb-4'><div class='profile_picture'><img src='https://josephchen.tech/wp-content/uploads/2020/08/test1.jpg'class='rounded-circle user_pfp'></div><div class='hid'><div class='message_received'>".$message."</div><div class='time_date'>".substr($resultv,0,strrpos($resultv,' ')).", ".$time."</div></div></div>";
 
 }
 echo $chat;
