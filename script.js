c01e8478586bbc38bbb9dc083f1f1cbceee69e0a
CoderStone <34324909+CoderStone@users.noreply.github.com>
2020-11-21 03:03:38 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function myFunction(num) {
-  scrollDown();
-}
-
-function scrollDown(){
-  var element = document.getElementById(11);
-  element.scrollTop = element.scrollHeight - element.clientHeight;
-}
