fc6efe0a3d210a42a8bb15658d7a632d804239c6
CoderStone <34324909+CoderStone@users.noreply.github.com>
2020-11-21 04:19:54 -0500

    O(1)post part 2

--- generateChatHTML.php
+++ generateChatHTML.php
@@ -3,25 +3,31 @@
 	if(!session_id())session_start();	
 	$email = $_SESSION['email'];
 	if(isset($_POST['userid'])){
-		$data = array('key'=>$key,'email'=>$_SESSION['email'],'id'=>$_POST['userid']);
-		$options = array(
-			'http' => array(
-				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-				'method'  => 'POST',
-				'content' => http_build_query($data)
-			)
-		);
-		$message=$_POST['content'];
-		$context  = stream_context_create($options);
-		$resultv = file_get_contents($spark."/verifyID", false, $context);
+	    $aid=$_POST['userid'];
+	    $atime=$_POST['time'];
+	    $acontent=$_POST['content'];
 		$chat="";
-		$time=$_POST['time'];
-		$time=date('m/d/Y',(int)$time/1000); 
-if($resultv==$_SESSION['name'])
-$chat="<div class='d-flex justify-content-end mb-4'><div><div class='message_sent'>".$message."</div><div class='time_date'>Me, "."  ".$time." </div></div><div class='profile_picture'><img src='https://cdn.discordapp.com/attachments/582274693097193472/778992941900627998/unknown.png'class='rounded-circle user_pfp'></div></div>";
-else
-$chat="<div class='d-flex justify-content-start mb-4'><div class='profile_picture'><img src='https://josephchen.tech/wp-content/uploads/2020/08/test1.jpg'class='rounded-circle user_pfp'></div><div class='hid'><div class='message_received'>".$message."</div><div class='time_date'>".substr($resultv,0,strrpos($resultv,' ')).", ".$time."</div></div></div>";
-
+	    for($x=0;$x<count($aid);$x++)
+	    {
+	        $idx=$aid[$x];
+	        $timex=(float)$atime[$x];
+		    $timex=date('M d, Y h:i:s',$timex/1000);
+	        $contentx=$acontent[$x];
+    		$data = array('key'=>$key,'email'=>$email,'id'=>$idx);
+    		$options = array(
+    			'http' => array(
+    				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+    				'method'  => 'POST',
+    				'content' => http_build_query($data)
+    			)
+    		);
+    		$context  = stream_context_create($options);
+    		$resultv = file_get_contents($spark."/verifyID", false, $context);
+            if($resultv==$_SESSION['name'])
+		    $chat=$chat."<div class='d-flex justify-content-end mb-4'><div><div class='message_sent'>".$contentx."</div><div class='time_date'>Me, "."  ".$timex." </div></div><div class='profile_picture'><img src='https://cdn.discordapp.com/attachments/582274693097193472/778992941900627998/unknown.png'class='rounded-circle user_pfp'></div></div>";
+		    else
+		        $chat=$chat."<div class='d-flex justify-content-start mb-4'><div class='profile_picture'><img src='https://josephchen.tech/wp-content/uploads/2020/08/test1.jpg'class='rounded-circle user_pfp'></div><div class='hid'><div class='message_received'>".$contentx."</div><div class='time_date'>".substr($resultv,0,strrpos($resultv,' ')).", ".$timex."</div></div></div>";
+	    }
 }
 echo $chat;
 ?>
